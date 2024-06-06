7613b63c95f642fa489dd770cdc1ac6df71efde7
Don Mahurin <2797413+dmahurin@users.noreply.github.com>
2024-06-06 10:35:35 -0700

    simplify.  Print the tokens when they are generated.

--- mlir_llm_runner.py
+++ mlir_llm_runner.py
@@ -1,30 +1,26 @@
 import argparse
 from turbine_models.model_runner import vmfbRunner
 from iree import runtime as ireert
-import torch
-import time
-from turbine_llamacpp.params import *
 from transformers import LlamaTokenizer
 
 parser = argparse.ArgumentParser()
 
-# TODO move common runner flags to generic flag file
 parser.add_argument(
     "--module",
     type=str,
-    default="output.vmfb",
+    default="module.vmfb",
     help="path to vmfb containing compiled module",
 )
 parser.add_argument(
     "--parameters",
     type=str,
-    default="reformatted_parameters.irpa",
+    default="parameters.irpa",
     help="path to external weight parameters",
 )
 parser.add_argument(
     "--tokenizer",
     type=str,
-    default="openlm-research/open_llama_3b",
+    default="BEE-spoke-data/smol_llama-101M-GQA",
     help="path to the hf model. Needed for tokenizer right now",
 )
 parser.add_argument(
@@ -40,8 +36,7 @@ parser.add_argument(
     help="prompt for llm model",
 )
 
-
-class SharkLLM(object):
+class LLM(object):
     def __init__(self, device, vmfb_path, external_weight_path):
         self.runner = vmfbRunner(
             device=device,
@@ -49,73 +44,36 @@ class SharkLLM(object):
             external_weight_path=external_weight_path,
         )
         self.model = self.runner.ctx.modules.llama_dpis
-        self.first_input = True
-        self.num_tokens = 0
-        self.last_prompt = None
-        self.prev_token_len = 0
 
     def format_out(self, results):
         return results.to_host()[0][0]
 
-    def generate(self, input_ids, tokenizer):
+    def initial_tokens(self, input_ids):
+        return ireert.asdevicearray(self.runner.config.device, input_ids)
+
+    def get_next_token(self, input_ids, initial):
         try:
-            turbine_results = []
-            # Only need not seen token for init cache
-            # Because we have stored the res in KV-cache.
-            token_len = input_ids.shape[-1]
-            inputs = [ireert.asdevicearray(self.runner.config.device, input_ids)]
-            s = time.time()
-            results = self.model["run_initialize"](*inputs)  # example_input_id
-            e = time.time()
-            print(
-                f"num_tokens: {token_len}, time_taken={e-s}, tok/second:{token_len/(e-s)}"
-            )
-            token_len += 1
-            self.first_input = False
-            s = time.time()
-            turbine_results.append(self.format_out(results))
-            while self.format_out(results) != 2:
-                print(results)
-                results = self.model["run_forward"](results)
-                # uncomment to see tokens as they are emitted
-                # print(f"turbine: {tokenizer.decode(self.format_out(results))}")
-                turbine_results.append(self.format_out(results))
-            e = time.time()
-            decoded_tokens = len(turbine_results)
-            print(
-                f"Decode num_tokens: {decoded_tokens}, time_taken={e-s}, tok/second:{decoded_tokens/(e-s)}"
-            )
-            self.prev_token_len = token_len + decoded_tokens
-            return turbine_results
+            if initial:
+                next_token = self.model["run_initialize"](input_ids)
+            else:
+                next_token = self.model["run_forward"](input_ids)
+            return next_token
         except KeyboardInterrupt:
-            return turbine_results
-
-
-def run_llm(
-    device,
-    prompt,
-    vmfb_path,
-    external_weight_path,
-    hf_model_path,
-):
-    tokenizer = LlamaTokenizer.from_pretrained(hf_model_path)
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-    llm = SharkLLM(
-        device=device,
-        vmfb_path=vmfb_path,
-        external_weight_path=external_weight_path,
-    )
-    print("generating turbine output: ")
-    return tokenizer.decode(llm.generate(input_ids, tokenizer=tokenizer))
-
+            return None
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    turbine_output = run_llm(
-        args.device,
-        args.prompt,
-        args.module,
-        args.parameters,
-        args.tokenizer,
+
+    tokenizer = LlamaTokenizer.from_pretrained(args.tokenizer)
+    input_ids = tokenizer(args.prompt, return_tensors="pt").input_ids
+    llm = LLM(
+        device=args.device,
+        vmfb_path=args.module,
+        external_weight_path=args.parameters,
     )
-    print(turbine_output)
+    tokens = llm.initial_tokens(input_ids)
+    initial = True
+    while True:
+        tokens = llm.get_next_token(tokens, initial)
+        initial = False
+        print(f"{tokenizer.convert_ids_to_tokens([llm.format_out(tokens)])[0].replace('▁', ' ')}", end='', flush=True)
